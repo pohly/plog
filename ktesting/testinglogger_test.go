9405f8edad47f2b2bef7caa3cea95c8278fbea44
Patrick Ohly <patrick.ohly@intel.com>
2022-06-29 13:23:07 +0200

    ktesting: stop using testing.T when test completes
    When testing.T.Log gets called after the test has completed, it panics. There's
    also a data race (https://github.com/kubernetes/kubernetes/issues/110854).
    
    Normally that should never happen because tests should ensure that all
    goroutines have stopped before returning. But sometimes it is not possible to
    do that. ktesting now automatically protects against that by registering a
    cleanup function and redirecting all future output into klog.

--- ktesting/testinglogger_test.go
+++ ktesting/testinglogger_test.go
@@ -10,9 +10,14 @@ package ktesting_test
 import (
 	"bytes"
 	"errors"
+	"flag"
 	"fmt"
+	"regexp"
+	"runtime"
+	"sync"
 	"testing"
 
+	"k8s.io/klog/v2"
 	"k8s.io/klog/v2/ktesting"
 )
 
@@ -132,6 +137,7 @@ func TestCallDepth(t *testing.T) {
 }
 
 type logToBuf struct {
+	ktesting.NopTL
 	bytes.Buffer
 }
 
@@ -146,4 +152,100 @@ func (l *logToBuf) Log(args ...interface{}) {
 		l.Write([]byte(fmt.Sprintf("%s", arg)))
 	}
 	l.Write([]byte("\n"))
+}
+
+func TestStop(t *testing.T) {
+	// This test is set up so that a subtest spawns a goroutine and that
+	// goroutine logs through ktesting *after* the subtest has
+	// completed. This is not supported by testing.T.Log and normally
+	// leads to:
+	//   panic: Log in goroutine after TestGoroutines/Sub has completed: INFO hello world
+	//
+	// It works with ktesting if (and only if) logging gets redirected to klog
+	// before returning from the test.
+
+	// Capture output for testing.
+	state := klog.CaptureState()
+	defer state.Restore()
+	var output bytes.Buffer
+	var fs flag.FlagSet
+	klog.InitFlags(&fs)
+	fs.Set("alsologtostderr", "false")
+	fs.Set("logtostderr", "false")
+	fs.Set("stderrthreshold", "FATAL")
+	fs.Set("one_output", "true")
+	klog.SetOutput(&output)
+
+	var logger klog.Logger
+	var line int
+	var wg1, wg2 sync.WaitGroup
+	wg1.Add(1)
+	wg2.Add(1)
+	t.Run("Sub", func(t *testing.T) {
+		logger, _ = ktesting.NewTestContext(t)
+		go func() {
+			defer wg2.Done()
+
+			// Wait for test to have returned.
+			wg1.Wait()
+
+			// This output must go to klog because the test has
+			// completed.
+			_, _, line, _ = runtime.Caller(0)
+			logger.Info("simple info message")
+			logger.Error(nil, "error message")
+			logger.WithName("me").WithValues("completed", true).Info("complex info message", "anotherValue", 1)
+		}()
+	})
+	// Allow goroutine above to proceed.
+	wg1.Done()
+
+	// Ensure that goroutine has completed.
+	wg2.Wait()
+
+	actual := output.String()
+
+	// Strip time and pid prefix.
+	actual = regexp.MustCompile(`(?m)^.* testinglogger_test.go:`).ReplaceAllString(actual, `testinglogger_test.go:`)
+
+	// All lines from the callstack get stripped. We can be sure that it was non-empty because otherwise we wouldn't
+	// have the < > markers.
+	//
+	// Full output:
+	// 	testinglogger_test.go:194] "TestStop/Sub leaked goroutine: WARNING: test kept at least one goroutine running after test completion" callstack=<
+	//        	goroutine 23 [running]:
+	//        	k8s.io/klog/v2/internal/dbg.Stacks(0x0)
+	//        		/nvme/gopath/src/k8s.io/klog/internal/dbg/dbg.go:34 +0x8a
+	//        	k8s.io/klog/v2/ktesting.tlogger.fallbackLogger({0xc0000f2780, {0x0, 0x0}, {0x0, 0x0, 0x0}})
+	//        		/nvme/gopath/src/k8s.io/klog/ktesting/testinglogger.go:292 +0x232
+	//        	k8s.io/klog/v2/ktesting.tlogger.Info({0xc0000f2780, {0x0, 0x0}, {0x0, 0x0, 0x0}}, 0x0, {0x5444a5, 0x13}, {0x0, ...})
+	//        		/nvme/gopath/src/k8s.io/klog/ktesting/testinglogger.go:316 +0x28a
+	//        	github.com/go-logr/logr.Logger.Info({{0x572438?, 0xc0000c0ff0?}, 0x0?}, {0x5444a5, 0x13}, {0x0, 0x0, 0x0})
+	//        		/nvme/gopath/pkg/mod/github.com/go-logr/logr@v1.2.0/logr.go:249 +0xd0
+	//        	k8s.io/klog/v2/ktesting_test.TestStop.func1.1()
+	//        		/nvme/gopath/src/k8s.io/klog/ktesting/testinglogger_test.go:194 +0xe5
+	//        	created by k8s.io/klog/v2/ktesting_test.TestStop.func1
+	//        		/nvme/gopath/src/k8s.io/klog/ktesting/testinglogger_test.go:185 +0x105
+	//         >
+	actual = regexp.MustCompile(`(?m)^\t.*?\n`).ReplaceAllString(actual, ``)
+
+	expected := fmt.Sprintf(`testinglogger_test.go:%d] "TestStop/Sub leaked goroutine: WARNING: test kept at least one goroutine running after test completion" callstack=<
+ >
+testinglogger_test.go:%d] "TestStop/Sub leaked goroutine: simple info message"
+testinglogger_test.go:%d] "TestStop/Sub leaked goroutine: error message"
+testinglogger_test.go:%d] "TestStop/Sub leaked goroutine/me: complex info message" completed=true anotherValue=1
+`,
+		line+1, line+1, line+2, line+3)
+	if actual != expected {
+		t.Errorf("Output does not match. Expected:\n%s\nActual:\n%s\n", expected, actual)
+	}
+
+	testingLogger, ok := logger.GetSink().(ktesting.Underlier)
+	if !ok {
+		t.Fatal("should have had a ktesting logger")
+	}
+	captured := testingLogger.GetBuffer().String()
+	if captured != "" {
+		t.Errorf("testing logger should not have captured any output, got instead:\n%s", captured)
+	}
 }
