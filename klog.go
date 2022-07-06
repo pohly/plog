c2d5a451f664994c87cfcf64e414d537a5892751
Patrick Ohly <patrick.ohly@intel.com>
2022-07-06 14:33:03 +0200

    contextual logging: enable by default again
    Commit 3c90bf9a79bf6ed7c82fa8cafeaf1681c8555255 accidentally changed the
    default for contextual logging from "enabled" to "disabled". The intention
    always was and still is to make the new API do something useful by default and
    only be more cautious in key Kubernetes binaries which have a feature gate.

--- klog.go
+++ klog.go
@@ -549,7 +549,11 @@ type loggingT struct {
 	vmap map[uintptr]Level
 }
 
-var logging loggingT
+var logging = loggingT{
+	settings: settings{
+		contextualLoggingEnabled: true,
+	},
+}
 
 // setVState sets a consistent state for V logging.
 // l.mu is held.
